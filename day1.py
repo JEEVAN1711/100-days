dcc13cc0a18ba67a4c3e711b94de4bcc36d5de72
JEEVAN1711 <jeevanmoorthi1711@gmail.com>
2024-08-01 21:36:36 +0530

    day1.py

--- day1.py
+++ day1.py
@@ -1,1 +1,7 @@
-a,b=
+In a park, they charge X rupees for adults and Y rupees for kids for entry. 
+The program must accept two integers A and B as the input(where A is the number of adults and B is the number of kids). 
+The program must print the total amount charged as the output.
+ANSWER:
+a,b=map(int,input().split())
+x,y=map(int,input().split())
+print(a*x+b*y)
