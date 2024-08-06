ee036577a18f18f10779b65078d853830d858cde
JEEVAN1711 <jeevanmoorthi1711@gmail.com>
2024-08-06 20:34:46 +0530

    day6.py

---
+++ day6.py
@@ -0,0 +1,6 @@
+get the input.the program must print the first character once the second character two times,the thitd character three times,the fouth character four times and the fifth character fives times as the output
+S=(input())
+count =1
+for a in S:
+  print(a*count,end="")
+  count+=1
