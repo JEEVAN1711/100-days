7d22cc19e83e84b7d4231818164204b99f60ac32
JEEVAN1711 <jeevanmoorthi1711@gmail.com>
2024-09-02 18:11:24 +0530

    day33.java

---
+++ day33.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int height = 9;
+        if(height >= 5 && height <= 10){
+            System.out.println(1);
+        }
+        else{
+            System.out.println(0);
+        }
+    }
+}
